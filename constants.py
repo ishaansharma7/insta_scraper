9c470b6909f4b2fc8c97b445975943917faa7cf5
Ishaan Sharma <ishaan.sharma@mycity4kids.com>
2022-10-17 12:22:45 +0530

    added username resp api

--- constants.py
+++ constants.py
@@ -9,9 +9,12 @@ CONSECUTIVE_FAIL_LIMIT = int(os.environ['CONSECUTIVE_FAIL_LIMIT'])
 SELENIUM_FAIL_LIMIT = int(os.environ['SELENIUM_FAIL_LIMIT'])
 
 
+BATCH_SIZE = int(os.environ['BATCH_SIZE'])
+
 API_BASE_URL=os.environ['API_BASE_URL']
 SEND_USERNAME_URL= API_BASE_URL + '/manager/send/user-names/'
 UPDATE_SCRAPEID_STATUS_URL = API_BASE_URL + '/manager/update/status/scrape-id/'
 SEND_SCRAPEID_URL = API_BASE_URL + '/manager/send/scrape-id/'
 REELS_DATA_URL = API_BASE_URL + '/manager/update/reels-data/'
 USER_DATA_URL = API_BASE_URL + '/manager/update/user-data/'
+USER_NAME_STATUS_URL = API_BASE_URL + '/manager/update/status/user-name/'
